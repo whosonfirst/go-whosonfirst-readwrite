a86946148e0ab7692c563817860da7929785dd8b
thisisaaroland <aaron@mapzen.com>
2017-12-16 09:53:55 -0500

    make the cache reader stuff work

--- reader/cache.go
+++ reader/cache.go
@@ -3,12 +3,14 @@ package reader
 import (
 	"github.com/whosonfirst/go-whosonfirst-readwrite/cache"
 	"io"
+	"log"
 )
 
 type CacheReader struct {
 	Reader
 	reader Reader
 	cache  cache.Cache
+	debug  bool
 }
 
 func NewCacheReader(r Reader, c cache.Cache) (Reader, error) {
@@ -16,6 +18,7 @@ func NewCacheReader(r Reader, c cache.Cache) (Reader, error) {
 	cr := CacheReader{
 		reader: r,
 		cache:  c,
+		debug:  false,
 	}
 
 	return &cr, nil
@@ -25,7 +28,16 @@ func (r *CacheReader) Read(key string) (io.ReadCloser, error) {
 
 	fh, err := r.cache.Get(key)
 
+	if r.debug {
+		log.Println("GET", key, fh, err)
+	}
+
 	if err == nil {
+
+		if r.debug {
+			log.Println("HIT", key)
+		}
+
 		return fh, nil
 	}
 
@@ -33,13 +45,25 @@ func (r *CacheReader) Read(key string) (io.ReadCloser, error) {
 		return nil, err
 	}
 
+	if r.debug {
+		log.Println("MISS", key)
+	}
+
 	fh, err = r.reader.Read(key)
+
+	if r.debug {
+		log.Println("READ", key, fh, err)
+	}
 
 	if err != nil {
 		return nil, err
 	}
 
 	fh, err = r.cache.Set(key, fh)
+
+	if r.debug {
+		log.Println("SET", key, fh, err)
+	}
 
 	if err != nil {
 		return nil, err
