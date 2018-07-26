cf11d30a6850b179d11c24b583f673428e931089
thisisaaronland <devnull@localhost>
2018-07-26 07:52:03 -0700

    just use ioutil.NopCloser

--- bytes/bytes.go
+++ bytes/bytes.go
@@ -2,16 +2,10 @@ package bytes
 
 import (
 	gobytes "bytes"
-	"io"
+	"io/ioutil"
 )
-
-type nopCloser struct {
-	io.Reader
-}
-
-func (nopCloser) Close() error { return nil }
 
 func ReadCloserFromBytes(b []byte) (io.ReadCloser, error) {
 	body := gobytes.NewReader(b)
-	return nopCloser{body}, nil
+	return ioutil.NopCloser(body)
 }
